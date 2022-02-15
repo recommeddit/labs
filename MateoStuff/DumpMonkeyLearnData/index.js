ebc4dee5856ed03d36beb8d555463d6dab17eff3
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-02-14 22:15:46 -0800

    Fix MonkeyLearn dumper and add dataset

--- MateoStuff/DumpMonkeyLearnData/index.js
+++ MateoStuff/DumpMonkeyLearnData/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const jetpack = require("fs-jetpack");
 const truncate = require("just-truncate");
 const { chromium } = require('playwright');
+const delay = require('delay');
 
 const MAX_LEN = 35;
 
@@ -16,14 +17,29 @@ const MAX_LEN = 35;
   const browser = await chromium.launch({ headless: process.env.USE_HEADLESS === "true" });
   console.log("Browser successfully launched!");
 
-  const skipAuth = await jetpack.existsAsync('auth.json');
+  const authFileExists = await jetpack.existsAsync('auth.json');
+  let authLastModified;
+  let envLastModified;
+  let skipAuth;
+  if (authFileExists) {
+    authLastModified = (await jetpack.inspectAsync("auth.json", { times: true })).modifyTime;
+    envLastModified = (await jetpack.inspectAsync(".env", { times: true })).modifyTime;
+    skipAuth = (authLastModified > envLastModified);
+  } else {
+    skipAuth = false;
+  }
 
   let context;
   if (skipAuth) {
-    console.log("auth.json file detected. Setting cookies and localstorage to it...");
+    console.log("auth.json file detected and up-to-date. Setting cookies and localstorage to it...");
     context = await browser.newContext({ storageState: "auth.json" });
   } else {
-    console.log("auth.json file missing! Generating empty browser context...");
+    if (!authFileExists) {
+      console.log("auth.json file missing!");
+    } else {
+      console.log(`auth.json file (${authLastModified}) out-of-date compared to .env (${envLastModified})...`);
+    }
+    console.log("Generating empty browser context...");
     context = await browser.newContext();
   }
 
@@ -41,6 +57,8 @@ const MAX_LEN = 35;
     await page.click('input:has-text("Login")');
   }
 
+  await page.click('text=My Models');
+
   // Click on model (e.g., Recommendation Extractor)
   const modelName = process.env.MODEL_NAME;
   console.log(`Clicking on the appropriate model (${modelName})`);
@@ -50,12 +68,27 @@ const MAX_LEN = 35;
   // e.g., /main/extractors/ex_Zcaa6vKA/ -> /main/extractors/ex_Zcaa6vKA/tab/build
   await page.click('text=Build');
 
+  const numLabeledExamples = Number(await page.innerText('[data-bind="number: numberOfStep"]'));
+
+  console.log(`I see there is a total of ${numLabeledExamples} labeled examples! I'll keep this in mind...`);
+
   // /main/extractors/ex_Zcaa6vKA/tab/build -> /main/extractors/ex_Zcaa6vKA/tab/data/
   await page.click('text=Data');
 
-  console.log("Selecting all data...");
+  console.log(`Selecting the oldest ${numLabeledExamples} examples...`);
+  await page.locator('text=Sort By').click();
+  await page.locator('text=Oldest').click();
+
+  console.log("Selecting current page data...");
+
+  console.log("Waiting three seconds for good measure...");
+
+  await delay(3000);
+
   // Select current page of data
-  await page.click('checkbox div');
+  await page.click('.select-all checkbox div');
+
+  console.log("Selecting all data...");
 
   // Select all data
   if (isHeadless) {
@@ -68,23 +101,21 @@ const MAX_LEN = 35;
   }
 
   console.log("Selected successfully! Starting logging...");
+
   // Start tagging
   await page.click('text=Actions');
   await page.click('text=Tag selected data');
 
-  const numExamples = Number(await page.innerText('[data-bind="text: selectedSamplesToGo"]'));
-  console.log(`Found a total of ${numExamples} entries in the dataset! Beginning to process them...`);
-
   const dataset = [];
 
-  for (let i = 0; i < numExamples; i++) {
+  const totalExamples = Number(await page.innerText('[data-bind="text: selectedSamplesToGo"]'));
+
+  for (let i = 0; i < numLabeledExamples; i++) {
     // Use a glob URL pattern
-    console.log(`\nExample ${i + 1} of ${numExamples}:`);
+    console.log(`\nExample ${i + 1} of ${numLabeledExamples}:`);
     console.log('Waiting on API for text and labels...');
-    const [res] = await Promise.all([
-      page.waitForResponse('**/sample_to_tag/**'),
-      page.click('text=SKIP'),
-    ]);
+    const [res] = await Promise.all([page.waitForResponse('**/sample_to_tag/**'), page.click(
+      'text=SKIP')]);
     console.log('Got the text and labels! Processing...');
     const { data } = await res.json();
     const { sample } = data;
@@ -92,8 +123,14 @@ const MAX_LEN = 35;
 
     dataset.push({
       text,
-      annotations: annotations.map(({ start_char: start_idx, end_char: end_idx }) =>
-        ({ text: text.substring(start_idx, end_idx), start_idx, end_idx }))
+      annotations: annotations.map(({
+                                      start_char: start_idx,
+                                      end_char: end_idx,
+                                    }) => ({
+        text: text.substring(start_idx, end_idx),
+        start_idx,
+        end_idx,
+      })),
     });
     console.log(`Successfully stored "${truncate(text, MAX_LEN).replace(/\n/g, "\\n")}" to array!`);
   }
@@ -105,15 +142,16 @@ const MAX_LEN = 35;
     console.log("Successfully wrote dataset to dataset.json!");
   })();
 
-  try {
-    console.log("Double-checking that we are at the end screen...");
-    await page.waitForSelector('text=All your data is tagged. Well done!', { timeout: 5000 });
-    console.log("It appears we are! Excellent. 😎")
-  } catch {
-    console.error("We didn't finish all the examples! ⚠️");
+  console.log("Double-checking that we have labeled the number of examples we expected...");
+  const numExamplesLeft = Number(await page.innerText('[data-bind="text: selectedSamplesToGo"]'));
+  if (totalExamples - numExamplesLeft !== numLabeledExamples) {
+    console.log("Something went wrong! We didn't label the expected number of examples!");
+    console.log(`Expected ${totalExamples - numLabeledExamples} examples left, but got ${numExamplesLeft} instead!`);
     console.log("Writing screenshot of current page to error.png...");
     await page.screenshot({ path: 'error.png' });
     console.log("Successfully wrote screenshot to error.png");
+  } else {
+    console.log("It appears we did! Excellent. 😎")
   }
 
   // ---------------------
--- MateoStuff/RedditAPITimeTrials/psaw_parser.py
+++ MateoStuff/RedditAPITimeTrials/psaw_parser.py
@@ -1,7 +1,7 @@
 from functional import seq
 from psaw import PushshiftAPI
 
-from utils import urls, url_to_subreddit, comment_to_dict, url_to_submission_id
+from utils import urls, url_to_subreddit, url_to_submission_id, comment_obj_to_dict
 
 api = PushshiftAPI()
 
@@ -10,7 +10,8 @@ def parse_with_psaw():
     comments = (seq(urls)
                 .map(lambda url: (url_to_submission_id(url), url_to_subreddit(url)))
                 .smap(lambda submission_id, subreddit:
-                      api.search_comments(subreddit=subreddit, link_id=submission_id))
-                .map(comment_to_dict)
+                      list(api.search_comments(subreddit=subreddit, link_id=submission_id)))
+                .flatten()
+                .map(comment_obj_to_dict)
                 .to_list())
     return comments
