a89a10370beee7cdb106171ab2cb390e879d7780
Aditya Ramakrishnan <adityarules2013@gmail.com>
2022-04-03 18:00:15 -0700

    added points calculation to scoring

--- AdityaStuff/scoring.py
+++ AdityaStuff/scoring.py
@@ -42,8 +42,3 @@ def calc_points(comment, upvotes):
 	arg = (2*sa_scores['pos'] + 1*sa_scores['neu'] - 3*sa_scores['neg'])*upvotes
 	points = sigmoid(arg)
 	return points
-
-if __name__ == "__main__":
-	comment = "TL;DR In large part I think school rankings are very course-grained. In the US, at least, the top 10-20 schools generally or in a particular category probably belong there, and the top 100 schools probably belong in that range, too. But the differences between the 2nd and the 5th or the 32nd and the 47th are not likely of any significance, at least not as measured by the rankings."
-	sentiments = get_sentiment_scores(comment)
-	import pdb;pdb.set_trace()
